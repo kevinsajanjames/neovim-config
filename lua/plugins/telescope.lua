52d50d5654980404ff75576895ec1ead93513a8b
Kevin Sajan James <kevinsajan@gmail.com>
2024-04-26 18:02:11 +0400

    Added todocomments plugin and some keymaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,7 +6,8 @@ return {
 		local builtin = require('telescope.builtin')
 		vim.keymap.set('n', '<leader>ff', builtin.find_files, {desc = "Find Files"})
 		vim.keymap.set('n', '<leader>fg', builtin.live_grep, {desc = "Live Grep"})
-		vim.keymap.set('n', '<leader>fb', builtin.buffers, {desc = "Buffer Search"})
+		vim.keymap.set('n', '<leader>fb', builtin.buffers, {desc = "Find In Open Buffers"})
+		vim.keymap.set('n', '<leader>fo', builtin.oldfiles, {desc = "Find In Recent Files"})
 		vim.keymap.set('n', '<leader>fh', builtin.help_tags, {desc = "Help Tags"})
 		end
     }
