b50d573886e6f7e833d14b366ebd3c56378de719
Kevin Sajan James <kevinsajan@gmail.com>
2024-04-27 14:21:26 +0400

    Made some changes to the UI, changed a few keymaps, changed to kanagawa theme.

--- lua/plugins/dashboard-nvim.lua
+++ lua/plugins/dashboard-nvim.lua
@@ -3,7 +3,45 @@ return {
 	event = "VimEnter",
 	config = function()
 		require("dashboard").setup({
-			-- config
+			theme = "hyper",
+			config = {
+				header = {
+					[[                                                     ]],
+					[[  __  __                 __  __                      ]],
+					[[ /\ \/\ \               /\ \/\ \  __                 ]],
+					[[ \ \ `\\ \     __    ___\ \ \ \ \/\_\    ___ ___     ]],
+					[[  \ \ , ` \  /'__`\ / __`\ \ \ \ \/\ \ /' __` __`\   ]],
+					[[   \ \ \`\ \/\  __//\ \L\ \ \ \_/ \ \ \/\ \/\ \/\ \  ]],
+					[[    \ \_\ \_\ \____\ \____/\ `\___/\ \_\ \_\ \_\ \_\ ]],
+					[[     \/_/\/_/\/____/\/___/  `\/__/  \/_/\/_/\/_/\/_/ ]],
+					[[                                                     ]],
+					[[                                                     ]],
+				},
+				footer = { " ", " ", "Ignorance is our Ammunition." },
+				shortcut = {
+					{ desc = "󰊳 Update", group = "@property", action = "Lazy update", key = "u" },
+					{
+						icon = " ",
+						icon_hl = "@variable",
+						desc = "Files",
+						group = "Label",
+						action = "Telescope find_files",
+						key = "f",
+					},
+					{
+						desc = " Apps",
+						group = "DiagnosticHint",
+						action = "Telescope app",
+						key = "a",
+					},
+					{
+						desc = " dotfiles",
+						group = "Number",
+						action = "Telescope dotfiles",
+						key = "d",
+					},
+				},
+			},
 		})
 	end,
 	dependencies = { { "nvim-tree/nvim-web-devicons" } },
