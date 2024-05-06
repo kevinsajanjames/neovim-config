e18065808dc0bd49244f6c4ce51aae7995b7ffd6
Kevin Sajan James <kevinsajan@gmail.com>
2024-05-06 13:54:11 +0400

    Added noice and changed lualine theme

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,6 +4,16 @@ return {
 	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
 		local builtin = require("telescope.builtin")
+		-- builtin.setup({
+		-- 	defaults = {
+		-- 		file_ignore_patterns = {
+		-- 			-- Files that telescope should ignore
+		-- 			"node_modules",
+		-- 			".venv",
+		-- 		},
+		-- 	},
+		-- })
+		--Keymaps
 		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find Files" })
 		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live Grep" })
 		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find In Open Buffers" })
